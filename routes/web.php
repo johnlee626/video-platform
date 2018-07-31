414d20a61a00f10ba5b76b194ede5d4a7e44da55
John Lee <john_lee@ahm.honda.com>
2018-07-30 17:04:42 -0700

    work on index page

--- routes/web.php
+++ routes/web.php
@@ -12,17 +12,8 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
-
-Route::get('/login', function () {
-    return view('login');
-});
-
-Route::get('/register', function () {
-    return view('register');
-});
-
 
 Auth::routes();
 
